2810b95a8725bfaf0d5b8f00f855fc758a7d8b96
absol2293 <axel.escoto@iteso.mx>
2024-05-11 09:28:55 -0600

    Show previous and incoming sessions for student

--- app/public/scripts/dashboard.js
+++ app/public/scripts/dashboard.js
@@ -114,3 +114,137 @@ if (accountType === "student") {
     <li><a href="./teacher.html"><i class='bx bx-group'></i>Teacher</a></li>
     `;
 }
+
+function loadData(){
+    if (accountType === "student") {
+        loadIncomingEnrolled();
+        loadPastEnrolled();
+    }
+    else if (accountType === "teacher") {
+        
+    }
+
+}
+
+/* 
+    Axel 5/11/2024
+    This function will fetch the incoming enrolled sessions (limited to 4 closest sessions)
+*/
+async function loadIncomingEnrolled(){
+    const token = sessionStorage.getItem("sToken");
+    const host = `https://proyecto-web-0bpb.onrender.com/sessions?`
+    const  incomingDiv = document.getElementById("incomingCards");
+
+    try {
+        //Get current datetime
+        const currentdate = new Date();
+        //Transform to string
+        const datetime = currentdate.toISOString();
+        console.log(datetime);
+        console.log(`${host}showenrolled=1&page=1&pagesize=4&from_date=${datetime}`);
+        const response = await fetch(`${host}showenrolled=1&page=1&pagesize=4&from_date=${datetime}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "x-auth": token,
+            },
+        });
+        const data = await response.json();
+        console.log(data);
+        if (response.ok) {
+            incomingDiv.innerHTML = "";
+            data.forEach((session) => {
+                incomingDiv.innerHTML += `
+                <li>
+                    <i class='bx bx-calendar-check'></i>
+                    <span class="info">
+                        <h3>
+                            ${session.subject}
+                        </h3>
+                        <p>${session.description}</p>
+                        <p><b>Where? :</b> ${session.location}</p>
+                        <p>${session.start}</p>
+                        <p>${session.end}</p>
+                    </span>
+                </li>
+                `;
+            });
+        }else{
+            Swal.fire({
+                title: "Error!",
+                text: data || "Failed to fetch sessions",
+                icon: "error",
+                confirmButtonText: "Ok",
+            });
+        }
+    }
+    catch (error) {
+        Swal.fire({
+            title: "Error!",
+            text: error || "Failed to fetch sessions",
+            icon: "error",
+            confirmButtonText: "Ok",
+        });
+    }
+}
+
+
+/* 
+    Axel 5/11/2024
+    This function will show the past enrolled sessions on the history tab
+*/
+async function loadPastEnrolled(){
+    const token = sessionStorage.getItem("sToken");
+    const host = `https://proyecto-web-0bpb.onrender.com/sessions?`
+    const  historyTable = document.getElementById("history");
+
+    try {
+        //Get current datetime
+        const currentdate = new Date();
+        //Transform to string
+        const datetime = currentdate.toISOString();
+        console.log(datetime);
+        console.log(`${host}showenrolled=1&page=1&pagesize=4&to_date=${datetime}`);
+        const response = await fetch(`${host}showenrolled=1&page=1&pagesize=4&to_date=${datetime}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "x-auth": token,
+            },
+        });
+        const data = await response.json();
+        console.log(data);
+        if (response.ok) {
+            historyTable.innerHTML = "";
+            data.forEach((session) => {
+                historyTable.innerHTML += `
+                <tr>
+                    <td>
+                        <img src="https://www.freeiconspng.com/uploads/business-man-with-clock-to-control-time-of-work-3.png">
+                        <p>${session.subject}</p>
+                    </td>
+                    <td>${session.start}<br>${session.end}</td>
+                    <td><span class="status ${session.status}">${session.status}</span></td>
+                </tr>
+                `;
+            });
+        }else{
+            Swal.fire({
+                title: "Error!",
+                text: data || "Failed to fetch sessions",
+                icon: "error",
+                confirmButtonText: "Ok",
+            });
+        }
+    }
+    catch (error) {
+        Swal.fire({
+            title: "Error!",
+            text: error || "Failed to fetch sessions",
+            icon: "error",
+            confirmButtonText: "Ok",
+        });
+    }
+}
+
+loadData();
