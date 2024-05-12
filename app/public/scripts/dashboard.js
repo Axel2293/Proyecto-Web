e2fbcb12256ade484c285d69e697d59aa6a02320
Reimound <jorge.figueroa@iteso.mx>
2024-05-11 20:11:17 -0600

    Alerts

--- app/public/scripts/dashboard.js
+++ app/public/scripts/dashboard.js
@@ -1,20 +1,20 @@
 const sideLinks = document.querySelectorAll(
-  ".sidebar .side-menu li a:not(.logout)"
+    ".sidebar .side-menu li a:not(.logout)"
 );
 
 // Hide sidebar when window is less than 768px
 if (window.innerWidth < 768) {
-  document.querySelector(".sidebar").classList.add("close");
+    document.querySelector(".sidebar").classList.add("close");
 }
 
 sideLinks.forEach((item) => {
-  const li = item.parentElement;
-  item.addEventListener("click", () => {
-    sideLinks.forEach((i) => {
-      i.parentElement.classList.remove("active");
+    const li = item.parentElement;
+    item.addEventListener("click", () => {
+        sideLinks.forEach((i) => {
+            i.parentElement.classList.remove("active");
+        });
+        li.classList.add("active");
     });
-    li.classList.add("active");
-  });
 });
 
 // const menuBar = document.querySelector(".content nav .bx.bx-menu");
@@ -22,15 +22,15 @@ const sideBar = document.querySelector(".sidebar");
 
 // if cursor not on top of the sidebar, hide it, else show it
 sideBar.addEventListener("mouseleave", () => {
-  sideBar.classList.add("close");
+    sideBar.classList.add("close");
 });
 sideBar.addEventListener("mouseenter", () => {
-  sideBar.classList.remove("close");
+    sideBar.classList.remove("close");
 });
 // show the sidebar at first, but hide it after some time
 const first = setTimeout(() => {
-  sideBar.classList.add("close");
-  clearTimeout(first);
+    sideBar.classList.add("close");
+    clearTimeout(first);
 }, 1750);
 
 // menuBar.addEventListener("click", () => {
@@ -58,15 +58,15 @@ const first = setTimeout(() => {
 // });
 
 window.addEventListener("resize", () => {
-  if (window.innerWidth < 768) {
-    sideBar.classList.add("close");
-  } else {
-    sideBar.classList.remove("close");
-  }
-  // if (window.innerWidth > 576) {
-  //     searchBtnIcon.classList.replace("bx-x", "bx-search");
-  //     searchForm.classList.remove("show");
-  // }
+    if (window.innerWidth < 768) {
+        sideBar.classList.add("close");
+    } else {
+        sideBar.classList.remove("close");
+    }
+    // if (window.innerWidth > 576) {
+    //     searchBtnIcon.classList.replace("bx-x", "bx-search");
+    //     searchForm.classList.remove("show");
+    // }
 });
 
 // const toggler = document.getElementById("theme-toggle");
@@ -80,60 +80,61 @@ window.addEventListener("resize", () => {
 // });
 
 document.addEventListener("DOMContentLoaded", function () {
-  // Logout button
-  const logoutBtn = document.querySelector(".sidebar .side-menu li a.logout");
-  logoutBtn.addEventListener("click", function () {
-    // redirect to route /home with sweetalert confirmation
-    Swal.fire({
-      title: "Are you sure?",
-      text: "You will be logged out",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Yes, log me out!",
-    }).then((result) => {
-      if (result.isConfirmed) {
-        window.location.href = "/";
-      }
+    // Logout button
+    const logoutBtn = document.querySelector(".sidebar .side-menu li a.logout");
+    logoutBtn.addEventListener("click", function () {
+        // redirect to route /home with sweetalert confirmation
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You will be logged out",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, log me out!",
+        }).then((result) => {
+            if (result.isConfirmed) {
+                window.location.href = "/";
+            }
+        });
     });
-  });
 });
 
 const sideMenu = document.querySelector(".side-menu");
 const accountType = sessionStorage.getItem("accountType");
 if (accountType === "student") {
-  sideMenu.innerHTML = `
+    sideMenu.innerHTML = `
     <li class="active"><a href="/dashboard"><i class='bx bxs-dashboard'></i>Dashboard</a></li>
     <li><a href="./session.html"><i class='bx bx-objects-horizontal-right'></i>Sessions</a></li>
     <li><a href="./enrolled.html"><i class='bx bxs-bookmark-alt'></i>Enrolled</a></li>
     `;
 } else if (accountType === "teacher") {
-  sideMenu.innerHTML = `
+    sideMenu.innerHTML = `
     <li class="active"><a href="/dashboard"><i class='bx bxs-dashboard'></i>Dashboard</a></li>
     <li><a href="./teacher.html"><i class='bx bx-group'></i>Teacher</a></li>
     `;
 }
 
 function loadData() {
-  const accountType = sessionStorage.getItem("accountType");
+    const accountType = sessionStorage.getItem("accountType");
 
-  loadIncoming(accountType);
-  loadPast(accountType);
+    loadIncoming(accountType);
+    loadPast(accountType);
+    loadAlerts();
 }
 
 function getDateTimeFormated(dStart, dEnd) {
-  // give format to the Hour
-  const start = dStart.split("T");
-  const end = dEnd.split("T");
-  const startHour = start[1].split(":");
-  const endHour = end[1].split(":");
-  const startHourFormat = `${startHour[0]}:${startHour[1]}`;
-  const endHourFormat = `${endHour[0]}:${endHour[1]}`;
+    // give format to the Hour
+    const start = dStart.split("T");
+    const end = dEnd.split("T");
+    const startHour = start[1].split(":");
+    const endHour = end[1].split(":");
+    const startHourFormat = `${startHour[0]}:${startHour[1]}`;
+    const endHourFormat = `${endHour[0]}:${endHour[1]}`;
 
-  const printable_hour = `${startHourFormat} - ${endHourFormat}`;
+    const printable_hour = `${startHourFormat} - ${endHourFormat}`;
 
-  return { start: start[0], end: end[0], printable_hour: printable_hour };
+    return { start: start[0], end: end[0], printable_hour: printable_hour };
 }
 
 /* 
@@ -141,54 +142,54 @@ function getDateTimeFormated(dStart, dEnd) {
     This function will fetch the incoming enrolled/created sessions (limited to 4 closest sessions)
 */
 async function loadIncoming(accountType) {
-  const token = sessionStorage.getItem("sToken");
-  let host = `https://proyecto-web-0bpb.onrender.com/sessions?`;
-  const incomingDiv = document.getElementById("incomingCards");
+    const token = sessionStorage.getItem("sToken");
+    let host = `https://proyecto-web-0bpb.onrender.com/sessions?`;
+    const incomingDiv = document.getElementById("incomingCards");
 
-  try {
-    //Get current datetime
-    const currentdate = new Date();
-    //Transform to string
-    const datetime = currentdate.toISOString();
-    console.log(datetime);
+    try {
+        //Get current datetime
+        const currentdate = new Date();
+        //Transform to string
+        const datetime = currentdate.toISOString();
+        console.log(datetime);
 
-    if (accountType === "student") {
-      host += "showenrolled=1&";
-      console.log("SHOW STUDENT ENROLLED");
-    } else if (accountType === "teacher") {
-      host += "showcreat=1&";
-      console.log("SHOW TEACHER CREATED");
-    }
-
-    console.log(`${host}page=1&pagesize=4&from_date=${datetime}`);
-    const response = await fetch(
-      `${host}page=1&pagesize=4&from_date=${datetime}`,
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          "x-auth": token,
-        },
-      }
-    );
-    const data = await response.json();
-    console.log(data);
-    if (response.ok) {
-      incomingDiv.innerHTML = "";
-      data.forEach((session) => {
-        // give format to the Hour
-        const dateFormated = getDateTimeFormated(session.start, session.end);
-
-        // Limit description to 25 characters, if it's longer, add "..."
-        if (session.description.length > 25) {
-          session.description = session.description.substring(0, 25) + "...";
+        if (accountType === "student") {
+            host += "showenrolled=1&";
+            console.log("SHOW STUDENT ENROLLED");
+        } else if (accountType === "teacher") {
+            host += "showcreat=1&";
+            console.log("SHOW TEACHER CREATED");
         }
 
-        if (session.subject.length > 14) {
-          session.subject = session.subject.substring(0, 25) + "...";
-        }
+        console.log(`${host}page=1&pagesize=4&from_date=${datetime}`);
+        const response = await fetch(
+            `${host}page=1&pagesize=4&from_date=${datetime}`,
+            {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    "x-auth": token,
+                },
+            }
+        );
+        const data = await response.json();
+        console.log(data);
+        if (response.ok) {
+            incomingDiv.innerHTML = "";
+            data.forEach((session) => {
+                // give format to the Hour
+                const dateFormated = getDateTimeFormated(session.start, session.end);
 
-        incomingDiv.innerHTML += `
+                // Limit description to 25 characters, if it's longer, add "..."
+                if (session.description.length > 25) {
+                    session.description = session.description.substring(0, 25) + "...";
+                }
+
+                if (session.subject.length > 16) {
+                    session.subject = session.subject.substring(0, 16) + "...";
+                }
+
+                incomingDiv.innerHTML += `
                 <li>
                     <i class='bx bx-calendar-check'></i>
                     <span class="info">
@@ -204,23 +205,23 @@ async function loadIncoming(accountType) {
                     </span>
                 </li>
                 `;
-      });
-    } else {
-      Swal.fire({
-        title: "Error!",
-        text: data || "Failed to fetch sessions",
-        icon: "error",
-        confirmButtonText: "Ok",
-      });
+            });
+        } else {
+            Swal.fire({
+                title: "Error!",
+                text: data || "Failed to fetch sessions",
+                icon: "error",
+                confirmButtonText: "Ok",
+            });
+        }
+    } catch (error) {
+        Swal.fire({
+            title: "Error!",
+            text: error || "Failed to fetch sessions",
+            icon: "error",
+            confirmButtonText: "Ok",
+        });
     }
-  } catch (error) {
-    Swal.fire({
-      title: "Error!",
-      text: error || "Failed to fetch sessions",
-      icon: "error",
-      confirmButtonText: "Ok",
-    });
-  }
 }
 
 /* 
@@ -228,49 +229,49 @@ async function loadIncoming(accountType) {
     This function will fetch the past enrolled/created sessions (limited to 4 closest sessions)
 */
 async function loadPast(accountType) {
-  const token = sessionStorage.getItem("sToken");
-  let host = `https://proyecto-web-0bpb.onrender.com/sessions?`;
-  const historyTable = document.getElementById("history");
+    const token = sessionStorage.getItem("sToken");
+    let host = `https://proyecto-web-0bpb.onrender.com/sessions?`;
+    const historyTable = document.getElementById("history");
 
-  try {
-    //Get current datetime
-    const currentdate = new Date();
-    //Transform to string
-    const datetime = currentdate.toISOString();
-    console.log(datetime);
+    try {
+        //Get current datetime
+        const currentdate = new Date();
+        //Transform to string
+        const datetime = currentdate.toISOString();
+        console.log(datetime);
 
-    if (accountType === "student") {
-      host += "showenrolled=1&";
-      console.log("SHOW STUDENT PAST ENROLLED");
-    } else if (accountType === "teacher") {
-      host += "showcreat=1&";
-      console.log("SHOW TEACHER PAST CREATED");
-    }
-
-    console.log(`${host}page=1&pagesize=4&to_date=${datetime}`);
-    const response = await fetch(
-      `${host}page=1&pagesize=4&to_date=${datetime}`,
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          "x-auth": token,
-        },
-      }
-    );
-    const data = await response.json();
-    console.log(data);
-    if (response.ok) {
-      historyTable.innerHTML = "";
-      data.forEach((session) => {
-        // give format to the date
-        const dateFormated = getDateTimeFormated(session.start, session.end);
-
-        if (session.subject.length > 14) {
-          session.subject = session.subject.substring(0, 25) + "...";
+        if (accountType === "student") {
+            host += "showenrolled=1&";
+            console.log("SHOW STUDENT PAST ENROLLED");
+        } else if (accountType === "teacher") {
+            host += "showcreat=1&";
+            console.log("SHOW TEACHER PAST CREATED");
         }
 
-        historyTable.innerHTML += `
+        console.log(`${host}page=1&pagesize=4&to_date=${datetime}`);
+        const response = await fetch(
+            `${host}page=1&pagesize=4&to_date=${datetime}`,
+            {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    "x-auth": token,
+                },
+            }
+        );
+        const data = await response.json();
+        console.log(data);
+        if (response.ok) {
+            historyTable.innerHTML = "";
+            data.forEach((session) => {
+                // give format to the date
+                const dateFormated = getDateTimeFormated(session.start, session.end);
+
+                if (session.subject.length > 14) {
+                    session.subject = session.subject.substring(0, 25) + "...";
+                }
+
+                historyTable.innerHTML += `
                 <tr>
                     <td>
                         <img src="https://www.freeiconspng.com/uploads/business-man-with-clock-to-control-time-of-work-3.png">
@@ -280,25 +281,89 @@ async function loadPast(accountType) {
                     <td><span class="status ${session.status}">${session.status}</span></td>
                 </tr>
                 `;
-      });
-    } else {
-      Swal.fire({
-        title: "Error!",
-        text: data || "Failed to fetch sessions",
-        icon: "error",
-        confirmButtonText: "Ok",
-      });
+            });
+        } else {
+            Swal.fire({
+                title: "Error!",
+                text: data || "Failed to fetch sessions",
+                icon: "error",
+                confirmButtonText: "Ok",
+            });
+        }
+    } catch (error) {
+        Swal.fire({
+            title: "Error!",
+            text: error || "Failed to fetch sessions",
+            icon: "error",
+            confirmButtonText: "Ok",
+        });
     }
-  } catch (error) {
-    Swal.fire({
-      title: "Error!",
-      text: error || "Failed to fetch sessions",
-      icon: "error",
-      confirmButtonText: "Ok",
-    });
-  }
 }
 
+async function loadAlerts() {
+    const token = sessionStorage.getItem("sToken");
+    const alertDiv = document.getElementById("alert-list");
 
+    try {
+        const response = await fetch(
+            `http://localhost:3151/users/alerts`,
+            {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    "x-auth": token,
+                },
+            });
+
+        if (response.headers.get("content-type").includes("application/json")) {
+            const data = await response.json();
+            console.log(data);
+            if (response.ok) {
+                alertDiv.innerHTML = "";
+                data.forEach((alert) => {
+                    const li = document.createElement('li');
+                    li.className = "completed";
+                    li.id = alert._id;
+                    li.innerHTML = `
+                        <div class="task-title">
+                            <i class="bx bx-check-circle"></i>
+                            <p>${alert.message}</p>
+                        </div>
+                        <i class="bx bx-dots-vertical-rounded"></i>
+                    `;
+                    li.addEventListener('click', async () => {
+                        try {
+                            const response = await fetch(`http://localhost:3151/users/alerts/${alert._id}`, {
+                                method: 'PUT',
+                                headers: {
+                                    "Content-Type": "application/json",
+                                    "x-auth": token,
+                                },
+                                body: JSON.stringify({ status: 'seen' })
+                            });
+                            if (response.ok) {
+                                location.reload();
+                            } else {
+                                console.error('Failed to update status');
+                            }
+                        } catch (error) {
+                            console.error(error);
+                        }
+                    });
+                    alertDiv.appendChild(li);
+                });
+            }
+        } else {
+            console.error("Response was not JSON");
+        }
+    } catch (error) {
+        Swal.fire({
+            title: "Error!",
+            text: error || "Failed to fetch alerts",
+            icon: "error",
+            confirmButtonText: "Ok",
+        });
+    }
+}
 
 loadData();
